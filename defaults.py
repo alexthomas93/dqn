a78fe56c71410c9cb492874c827f8c6a048a0425
Alex Thomas <alexthomas93@users.noreply.github.com>
2018-03-27 20:53:32 +0100

    Added defaults.py

---
+++ defaults.py
@@ -0,0 +1,13 @@
+env = "CartPole-v1"
+learning_rate = 0.001
+batch_size = 32
+decay_delay = 0
+discount_factor = 0.99
+episodes = 500
+epsilon = 1.0
+epsilon_decay = 0.995
+epsilon_min = 0.01
+memory_len = 2000
+update_rate = 1
+model="CartPole-v1"
+filename="CartPole-v1"
